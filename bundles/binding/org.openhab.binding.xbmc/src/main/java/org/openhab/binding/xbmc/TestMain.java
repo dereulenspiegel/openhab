b033481e35afe186bf2e33a2f06b767c7a8cd3d2
Till Klocke <till.klocke@gmail.com>
2013-10-08 13:17:19 +0200

    TestMain class will compile again

--- bundles/binding/org.openhab.binding.xbmc/src/main/java/org/openhab/binding/xbmc/TestMain.java
+++ bundles/binding/org.openhab.binding.xbmc/src/main/java/org/openhab/binding/xbmc/TestMain.java
@@ -1,9 +1,6 @@
 package org.openhab.binding.xbmc;
 
 import org.openhab.binding.xbmc.internal.client.XBMCClient;
-import org.openhab.binding.xbmc.internal.client.XBMCClient.XBMCListener;
-import org.openhab.binding.xbmc.internal.client.messages.data.PlayPauseStopData;
-import org.openhab.binding.xbmc.internal.tcp.XBMCSocketException;
 
 public class TestMain {
 
@@ -13,41 +10,5 @@ public class TestMain {
 	public static void main(String[] args) {
 		System.out.println("Connecting to XBMC");
 		XBMCClient client = new XBMCClient();
-		try {
-			client.registerPlayListener(new XBMCListener() {
-
-				@Override
-				public void onStop(PlayPauseStopData playPauseStopData) {
-					// TODO Auto-generated method stub
-
-				}
-
-				@Override
-				public void onPlay(PlayPauseStopData playPauseData) {
-					System.out.println("XBMC is playing something");
-					System.out.println("XBMC is playing a " + playPauseData.getItem().getType());
-
-				}
-
-				@Override
-				public void onPause(PlayPauseStopData playPauseData) {
-					// TODO Auto-generated method stub
-
-				}
-			});
-			client.open("192.168.100.50", 9090);
-			System.out.println("Successfully connected to XBMC");
-			while (true) {
-				try {
-					Thread.sleep(100);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		} catch (XBMCSocketException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	}
 }
