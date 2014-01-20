322d2dacfb5e3a6d1720847ec284531daf96af01
Till Klocke <till.klocke@gmail.com>
2014-01-20 08:39:54 +0100

    Added test for binding provder in EM binding

---
+++ bundles/binding/org.openhab.binding.em.test/src/test/java/org/openhab/binding/em/internal/BindingProviderTest.java
@@ -0,0 +1,43 @@
+package org.openhab.binding.em.internal;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openhab.binding.em.internal.EMBindingConfig.Datapoint;
+import org.openhab.binding.em.internal.EMBindingConfig.EMType;
+import org.openhab.core.items.Item;
+import org.openhab.core.library.items.NumberItem;
+import org.openhab.model.item.binding.BindingConfigParseException;
+
+public class BindingProviderTest {
+
+	private EMGenericBindingProvider provider;
+
+	@Before
+	public void before() {
+		provider = new EMGenericBindingProvider();
+	}
+
+	@Test
+	public void testSuccessfullParsing() throws Exception {
+		String itemConfig = "type=01;address=BC;datapoint=CUMULATED_VALUE;correctionFactor=400";
+		Item emItem = new NumberItem("emItem");
+		provider.processBindingConfiguration("", emItem, itemConfig);
+		EMBindingConfig config = provider.getConfigByTypeAndAddressAndDatapoint(EMType.EM1000S, "BC",
+				Datapoint.CUMULATED_VALUE);
+		Assert.assertNotNull(config);
+	}
+
+	@Test(expected = BindingConfigParseException.class)
+	public void testUnknownEMType() throws Exception {
+		String itemConfig = "type=04|address=BC|datapoint=LAST_VALUE;correctionFactor=400";
+		processBindingConfig(itemConfig);
+	}
+
+	private void processBindingConfig(String itemConfig) throws Exception {
+		Item emItem = new NumberItem("emItem");
+		provider.processBindingConfiguration("", emItem, itemConfig);
+	}
+
+}
